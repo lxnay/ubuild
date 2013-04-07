01d1eab113fcea94ca56d4ad4d962289acebcaad
Fabio Erculiani <lxnay@sabayon.org>
2013-04-07 12:58:41 +0100

    scripts: pre_build: rsync WORK_INITRAMFS_ROOTFS_DIR from initramfs_rootfs_dir=

--- specs/scripts/pre_build.sh
+++ specs/scripts/pre_build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -e
-
-. build.include
+. build.include || exit 1
 
 echo "Mirroring ${UBUILD_ROOTFS_DIR} into work rootfs dir: ${WORK_ROOTFS_DIR}/"
 
-mkdir -p "${WORK_ROOTFS_DIR}"
+mkdir -p "${WORK_ROOTFS_DIR}" || exit 1
 /usr/bin/rsync -a -x -H -A -X --delete-during \
-    "${UBUILD_ROOTFS_DIR}"/ "${WORK_ROOTFS_DIR}"/
+    "${UBUILD_ROOTFS_DIR}"/ "${WORK_ROOTFS_DIR}"/ || exit 1
+
+echo -n "Mirroring ${UBUILD_INITRAMFS_ROOTFS_DIR} into work initramfs"
+echo "rootfs dir: ${WORK_INITRAMFS_ROOTFS_DIR}/"
+
+mkdir -p "${WORK_INITRAMFS_ROOTFS_DIR}" || exit 1
+/usr/bin/rsync -a -x -H -A -X --delete-during \
+    "${UBUILD_INITRAMFS_ROOTFS_DIR}"/ \
+    "${WORK_INITRAMFS_ROOTFS_DIR}"/ || exit 1
